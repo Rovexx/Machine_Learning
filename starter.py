2902890620ebe7776891bfa98ef12f3de77b05e9
Rovexx <roel.versteeg@hotmail.com>
2020-04-06 17:32:58 +0200

    Decision tree unsupvised learning

--- starter.py
+++ starter.py
@@ -17,6 +17,11 @@ from sklearn.metrics import accuracy_score
 from sklearn.cluster import KMeans
 from sklearn.datasets import make_blobs
 
+# Decision tree en logistic regression
+from sklearn import tree
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+
 def extract_from_json_as_np_array(key, json_data):
     """ helper functie om data uit de json te halen en om te zetten naar numpy array voor sklearn"""
     data_as_array = []
@@ -26,8 +31,6 @@ def extract_from_json_as_np_array(key, json_data):
     return np.array(data_as_array)
 
 STUDENTNUMMER = "0940512"
-
-print("STARTER CODE")
 
 # maak een data-object aan om jouw data van de server op te halen
 data = Machine_Learning_Data(STUDENTNUMMER)
@@ -64,11 +67,6 @@ plt.scatter(centroids[:, 0], centroids[:, 1], marker = "x", s = 150, linewidths 
 
 plt.show()
 
-# TODO: print deze punten uit en omcirkel de mogelijke clusters
-
-
-# TODO: ontdek de clusters mbv kmeans en teken een plot met kleurtjes
-
 
 # SUPERVISED LEARNING
 
@@ -77,33 +75,52 @@ classification_training = data.classification_training()
 
 # extract de data x = array met waarden, y = classificatie 0 of 1
 X = extract_from_json_as_np_array("x", classification_training)
-
 # dit zijn de werkelijke waarden, daarom kan je die gebruiken om te trainen
 Y = extract_from_json_as_np_array("y", classification_training)
 
 
-# TODO: leer de classificaties
+# leer de classificaties
+x_2 = X[...,0]
+y_2 = X[...,1]
 
-# TODO: voorspel na het trainen de Y-waarden (je gebruikt hiervoor dus niet meer de
-#       echte Y-waarden, maar enkel de X en je getrainde classifier) en noem deze
-#       bijvoordeeld Y_predict
+for i in range(len(x_2)):
+    plt.plot(x_2[i], y_2[i], 'k.')
 
-# TODO: vergelijk Y_predict met de echte Y om te zien hoe goed je getraind hebt
+plt.axis([min(x_2), max(x_2), min(y_2), max(y_2)])
 
+# toon alle punten zonder classificatie
+plt.show()
+
+# DECISION TREE
+# train deze punten in een decision tree
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(X, Y)
+
+# voorspel na het trainen de Y-waarden (je gebruikt hiervoor dus niet meer de
+#   echte Y-waarden, maar enkel de X en je getrainde classifier) en noem deze
+#   bijvoordeeld Y_predict
+Y_predict = clf.predict(X)
+clf_score = accuracy_score(Y, Y_predict)
+print("Decision tree accuratie (score): " + str(clf_score))
+
+# vergelijk Y_predict met de echte Y om te zien hoe goed je getraind hebt
+plt.scatter(x_2, y_2, c = Y_predict, s = 10)
+plt.show()
 
 # haal data op om te testen
 classification_test = data.classification_test()
 # testen doen we 'geblinddoekt' je krijgt nu de Y's niet
 X_test = extract_from_json_as_np_array("x", classification_test)
 
-# TODO: voorspel na nog een keer de Y-waarden, en plaats die in de variabele Z
-#       je kunt nu zelf niet testen hoe goed je het gedaan hebt omdat je nu
-#       geen echte Y-waarden gekregen hebt.
-#       onderstaande code stuurt je voorspelling naar de server, die je dan
-#       vertelt hoeveel er goed waren.
-
-Z = np.zeros(100) # dit is een gok dat alles 0 is... kan je zelf voorspellen hoeveel procent er goed is?
+# voorspel na nog een keer de Y-waarden, en plaats die in de variabele Z
+# je kunt nu zelf niet testen hoe goed je het gedaan hebt omdat je nu
+# geen echte Y-waarden gekregen hebt.
+# onderstaande code stuurt je voorspelling naar de server, die je dan
+# vertelt hoeveel er goed waren.
+Y_predict = clf.predict(X_test)
+Z = Y_predict
+# Z = np.zeros(100) # dit is een gok dat alles 0 is... kan je zelf voorspellen hoeveel procent er goed is?
 
 # stuur je voorspelling naar de server om te kijken hoe goed je het gedaan hebt
 classification_test = data.classification_test(Z.tolist()) # tolist zorgt ervoor dat het numpy object uit de predict omgezet wordt naar een 'normale' lijst van 1'en en 0'en
-print("Classificatie accuratie (test): " + str(classification_test))
+print("Classificatie accuratie (decisionTree): " + str(classification_test))
